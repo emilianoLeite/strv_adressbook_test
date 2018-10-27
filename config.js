a6135d8470ad40b2aafc8ccd537bbd69e3f4f57f
Emiliano Leite <emiliano.leite@dev.codus.com.br>
2018-10-27 21:05:45 -0200

    Install dotenv and firebase

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
+require('dotenv').config();
 const logger = require('morgan');
+
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
 
 module.exports = function (app) {
   switch (process.env.NODE_ENV) {
@@ -14,4 +19,22 @@ module.exports = function (app) {
       app.use(logger('dev'));
       break;
   }
+
+  const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_SENDER_ID
+  };
+  firebase.initializeApp(config);
+
+  const db = firebase.firestore();
+  // Disable deprecated features
+  db.settings({
+    timestampsInSnapshots: true
+  });
+
+  app.set('db', db);
 }
