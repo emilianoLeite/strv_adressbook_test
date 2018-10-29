aee1c36d5f7ba00787d1fbeeabe1758c8f143d31
Emiliano Leite <emiliano.leite@dev.codus.com.br>
2018-10-29 20:16:11 -0200

    Add email format validation to User model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, index: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    index: true,
+    unique: true,
+    validate: {
+      validator: function validEmail(v) {
+        return /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(v)
+      },
+      message: props => `${props.value} is not a valid email!`
+    }
+  },
   password: { type: String, required: true }
 });
 
