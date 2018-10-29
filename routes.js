e744c79f38e1ebfce86b87b64d3ed7fe5f01b4b5
Emiliano Leite <emiliano.leite@dev.codus.com.br>
2018-10-29 20:20:18 -0200

    Register registrations endpoints in routes helper

--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
 module.exports = (app) => {
   const contactsController = require('./controllers/contacts');
+  const registrationsController = require('./controllers/registrations');
 
   app.get('/', (request, response) => {
     response.sendStatus(200);
   });
 
   contactsController(app);
+  registrationsController(app);
 };
