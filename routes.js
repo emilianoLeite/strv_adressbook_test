81c69df8eba43a842a31b6d7dad0741a06bb02c4
Emiliano Leite <emiliano.leite@dev.codus.com.br>
2018-11-01 22:32:26 -0200

    Remove '/´ route
    It was only used for testing purposes.

--- routes.js
+++ routes.js
@@ -2,10 +2,6 @@ module.exports = (app) => {
   const contactsController = require('./controllers/contacts');
   const registrationsController = require('./controllers/registrations');
 
-  app.get('/', (request, response) => {
-    response.sendStatus(200);
-  });
-
   contactsController(app);
   registrationsController(app);
 };
